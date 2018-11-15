c84e08e7930b4a5f07960cf43f921c19aa8d8a3d
苏晓晓 <suxx@dilusense.com>
2018-11-15 19:18:21 +0800

    modify

--- src/lib/List_comment.vue
+++ src/lib/List_comment.vue
@@ -1,28 +1,15 @@
 <template>
-  <div class="div-relative">
-    <div class="div-b" style="text-align:center">
-      <img src="../assets/up.png" height="30" width="30" @click="add"/>
-    </div>
-    <div class="clean"></div>
-    <div class="content">
-      <ul>
-        <transition-group name="list-complete" tag="p">
-          <li class="list-complete-item" v-bind:key="item.user_id" v-for="(item,index) in TableData">
-            <img v-if="show" class="img" :src="item.profile_image" :width="height/TableLen"
-                 :height="height/TableLen-4"
-                 :onerror="img404"
-            />
-            {{item.name}}
-          </li>
-        </transition-group>
-      </ul>
-
-    </div>
-    <div class="div-c  " style="text-align:center">
-      <img src="../assets/down.png" height="30" width="30" @click="below"/>
-    </div>
+  <div class="content">
+    <ul>
+      <transition-group name='flip-list' tag='ul' mode='in-out'>
+        <li class='flip-list-item' :key="item.id" v-for="(item,index) in TableData">
+          <img v-if="show" class="img" :src="item.image" :width="height/TableLen"
+               :height="height/TableLen-4"/>
+          {{item.name}}
+        </li>
+      </transition-group>
+    </ul>
   </div>
-
 </template>
 
 <script>
@@ -34,17 +21,16 @@
         default: []
       },
       TableLen: Number,
-
     },
     data: function () {
       return {
         msg: '',
         height: 0,
-        // singerData: [],
         ArrayStartIndex: 0,
         PlayTimer: Number,
         show: true,
-        img404: "this.onerror='';this.src='" + require('../assets/logo.png') + "'",
+        chakanlist: [],
+        length1: this.singerData.length
       }
     },
     computed: {
@@ -53,6 +39,7 @@
       TableData: function () {
         if (this.$el) this.height = this.$el.parentElement.offsetHeight;
         let data = this.singerData.slice(this.ArrayStartIndex, this.ArrayStartIndex + this.TableLen);
+        console.log(data.length);
         if (data.length < this.TableLen) {
           data.push.apply(data, this.singerData.slice(0, this.TableLen - data.length));
         }
@@ -71,9 +58,6 @@
         clearTimeout(this.PlayTimer);
         this.autoPlay();
       },
-      rem: function () {
-        this.singerData.splice(this.ArrayStartIndex, 1)
-      },
       autoPlay() {
         let length = this.singerData.length;
         if (this.ArrayStartIndex >= length) {
@@ -88,13 +72,10 @@
       },
     },
     mounted() {
-
-      //this.height = this.$el.parentElement.offsetHeight;
-      this.autoPlay();
+        this.autoPlay();
     },
     created() {
     }
-
   }
 </script>
 
@@ -109,57 +90,36 @@
     margin: auto;
   }
 
-  .list-enter-active, .list-leave-active {
-    /*transition: all 1s;*/
-    /*transition: opacity .1s*/
+  .flip-list-item {
+    list-style-type: none;
+    /**
+       * 可以在v-enter-active和v-move中分别用transition过渡，也可以在item中用transition，包含了这两项
+       * 要用all不用transform，有可能是因为splice删除效果不是transform
+       */
+    /* transition: all 1s; */
   }
 
-  .list-enter, .list-leave-to
-    /* .list-leave-active for below version 2.1.8 */
-  {
-    opacity: 0;
-    transform: translateY(0px);
+  .flip-list-leave-active {
+    transition: all .5s;
   }
 
-  .list-complete-item {
+  .flip-list-enter-active {
     transition: all 1s;
-    margin-right: 10px;
   }
 
-  .list-complete-enter, .list-complete-leave-to {
+  .flip-list-move {
+    transition: transform 1s;
+  }
+
+  .flip-list-enter, .flip-list-leave-to {
     opacity: 0;
-    transform: translateY(30px);
+    transform: translateY(0);
   }
 
-  .list-complete-leave-active {
+  /**
+   * 要让删除的元素先脱离文档流，旁边的元素才能过渡过来
+   */
+  .flip-list-leave-active {
     position: absolute;
-  }
-
-  .clean {
-    clear: both;
-  }
-
-  .div-b {
-    position: absolute;
-    /**div居中**/
-    margin: auto;
-    width: 300px;
-    opacity: 0.50; /**其他浏览器（0.5为透明度，在0-1之间的浮点数）**/
-    filter: alpha(opacity=40);
-    /*display: none;*/
-  }
-
-  .div-c {
-    position: absolute;
-    /**div居中**/
-    margin: auto;
-    bottom: 2px;
-    width: 300px;
-    opacity: 0.50; /**其他浏览器（0.5为透明度，在0-1之间的浮点数）**/
-    filter: alpha(opacity=40);
-    /*display: none;*/
-  }
-.div-relative{
-  position:relative;
   }
 </style>
--- src/pages/Scroll.vue
+++ src/pages/Scroll.vue
@@ -1,8 +1,17 @@
 <template>
-  <div class="div_list">
-    <list_comment :singerData="singerData"
-                  :TableLen="6"></list_comment>
+  <div>
+    <div class="div_list">
+      <list_comment :singerData="singerData"
+                    :TableLen="6" ref="list_com"></list_comment>
+    </div>
+    <div class="div_1">
+      <button @click="up">上一张</button>
+      <button @click="below1">下一张</button>
+      <button @click="add1">添加</button>
+      <button @click="delect1">删除</button>
+    </div>
   </div>
+
 
 </template>
 
@@ -11,20 +20,20 @@
 
   export default {
     name: 'Scroll',
-    components: { List_comment},
+    components: {List_comment},
     data() {
       return {
         invTime: 2000,
-        singerData: [{"index":1,"image": "http://img15.3lian.com/2015/f2/8/d/96.jpg", "name": "一"},
-          {"index":2,"image": "http://img15.3lian.com/2015/f2/8/d/97.jpg", "name": "二"},
-          {"index":3,"image": "http://img15.3lian.com/2015/f2/8/d/98.jpg", "name": "三"},
-          {"index":4,"image": "http://img15.3lian.com/2015/f2/8/d/99.jpg", "name": "四"},
-          {"index":5,"image": "http://img15.3lian.com/2015/f2/8/d/111.jpg", "name": "五"},
-          {"index":6,"image":"http://img15.3lian.com/2015/f2/8/d/2.jpg","name":"67777777"},
-          {"index":7,"image":"http://img15.3lian.com/2015/f2/8/d/22.jpg","name":"4444444"},
-          {"index":8,"image":"http://img15.3lian.com/2015/f2/8/d/11.jpg","name":"222222"},
+        singerData: [{"id": 1, "image": "http://img15.3lian.com/2015/f2/8/d/96.jpg", "name": "测试1"},
+          {"id": 2, "image": "http://img15.3lian.com/2015/f2/8/d/97.jpg", "name": "测试2"},
+          {"id": 3, "image": "http://img15.3lian.com/2015/f2/8/d/98.jpg", "name": "测试3"},
+          {"id": 4, "image": "http://img15.3lian.com/2015/f2/8/d/99.jpg", "name": "测试4"},
+          {"id": 5, "image": "http://img15.3lian.com/2015/f2/8/d/111.jpg", "name": "测试5"},
+          {"id": 6, "image": "http://img15.3lian.com/2015/f2/8/d/2.jpg", "name": "测试6"},
+          {"id": 7, "image": "http://img15.3lian.com/2015/f2/8/d/22.jpg", "name": "测试7"},
+          {"id": 8, "image": "http://img15.3lian.com/2015/f2/8/d/11.jpg", "name": "测试8"},
         ],
-        list: []
+        id: 0
       }
     }, methods: {
       refresh() {
@@ -46,15 +55,28 @@
           }, 1000)
         })
       },
-      add:function () {
-        var list=[{
-          "image":"http://img15.3lian.com/2015/f2/8/d/2.jpg",
-          "name":"数据1"
-        },{
-          "image":"http://img15.3lian.com/2015/f2/8/d/3.jpg",
-          "name":"数据2"
-        }]
-        this.singerData=this.singerData.concat(list);
+      up: function () {
+        this.$refs.list_com.add();
+      },
+      below1: function () {
+        this.$refs.list_com.below();
+      },
+      add1: function () {
+        var lie = this.singerData[this.singerData.length - 1];
+        console.log("数组" + this.singerData.length);
+        this.id++;
+        let _id = this.id + 8;
+        var list = {"id": 8, "image": "http://img15.3lian.com/2015/f2/8/d/11.jpg", "name": "测试8"};
+        list.id = this.id + 8;
+        list.image = "http://img15.3lian.com/2015/f2/8/d/" + _id+".jpg";
+        list.name = "测试" + _id;
+        console.log(list);
+        this.singerData.push(list);
+        console.log(this.singerData);
+      },
+      delect1: function () {
+        this.singerData.pop();
+        console.log(this.singerData)
       }
     }
     , created() {
@@ -75,14 +97,14 @@
       //     });
       //   }, 1000)
       // }
-      var that = this;
-        var url = "https://www.apiopen.top/satinApi?type=1&page=1";
-          this.$ajax(url).then(function (res) {
-            console.log(url)
-            console.log(res);
-            that.singerData = res.data.data;
-            that.list = that.singerData;
-          })
+      // var that = this;
+      // var url = "https://www.apiopen.top/satinApi?type=1&page=1";
+      // this.$ajax(url).then(function (res) {
+      //   console.log(url)
+      //   console.log(res);
+      //   that.singerData = res.data.data;
+      //   that.list = that.singerData;
+      // })
     },
     destroyed() {
       clearInterval(this.timer);//页面销毁时清除定时器
@@ -101,5 +123,9 @@
     /*-moz-osx-font-smoothing: grayscale;*/
     /*color: #2c3e50;*/
   }
-</style>
 
+  .div_1 {
+    margin: 0 auto;
+    width: 300px;
+  }
+</style>
